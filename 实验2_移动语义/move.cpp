a7321b1a6e48aa3253e8a1c13456b9312061b243
D <563131295@qq.com>
2023-10-21 22:13:42 +0800

    移动语义

---
+++ 实验2_移动语义/move.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string.h>
+#include <sstream>
+#include <stdio.h>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+
+class Profiler//计时工具类
+{
+public:
+	Profiler(const string& tag)
+		: m_Tag(tag)
+		, m_StartTime(chrono::high_resolution_clock::now())
+	{}
+	~Profiler()
+	{
+		auto endTime = chrono::high_resolution_clock::now();
+		auto duration = chrono::duration_cast<chrono::milliseconds>(endTime - m_StartTime).count();
+		cout << m_Tag << " 耗时: " << duration << " ms" << endl;
+	}
+private:
+	string m_Tag;
+	chrono::steady_clock::time_point m_StartTime;
+};
+template<typename T>
+void moveData(T& dst, T& src)
+{
+	dst = std::move(src);
+}
+template<typename T>
+void copyData(T& dst, T& src)
+{
+	dst = src;
+}
+int main()
+{
+	vector<int> d1;
+	{//移动语义代码块
+		vector<int> s1(100000);
+		auto profiler = Profiler("move");
+		for (int i = 0; i < 1000; ++i)
+		{
+			moveData(d1, s1);
+			moveData(s1, d1);
+		}
+	}
+	vector<int> d2;
+	{//拷贝语义代码块
+		vector<int> s2(100000);
+		auto profiler = Profiler("copy");
+		for (int i = 0; i < 1000; ++i)
+		{
+			copyData(d2, s2);
+			copyData(s2, d2);
+		}
+	}
+}
