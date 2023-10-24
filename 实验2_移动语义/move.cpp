0436ab6c48ca577640384a18b84f41d07b45b978
D <563131295@qq.com>
2023-10-24 23:09:44 +0800

    修正

--- 实验2_移动语义/move.cpp
+++ 实验2_移动语义/move.cpp
@@ -31,6 +31,11 @@ void moveData(T& dst, T& src)
 	dst = std::move(src);
 }
 template<typename T>
+void moveData(T& dst,T&& src)
+{
+	dst = src;
+}
+template<typename T>
 void copyData(T& dst, T& src)
 {
 	dst = src;
@@ -45,6 +50,16 @@ int main()
 		{
 			moveData(d1, s1);
 			moveData(s1, d1);
+		}
+	}
+	vector<int> d11;
+	{//移动语义代码块
+		vector<int> s11(100000);
+		auto profiler = Profiler("fail move");
+		for (int i = 0; i < 1000; ++i)
+		{
+			moveData(d11, std::move(s11));
+			moveData(s11, std::move(d11));
 		}
 	}
 	vector<int> d2;
