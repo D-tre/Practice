259d9c6cfec532e0ccaf8b90573bb2c5e30b2252
Li <563131295@qq.com>
2024-09-13 22:25:56 +0800

    重新写

--- 打家劫舍/打家劫舍.cpp
+++ 打家劫舍/打家劫舍.cpp
@@ -1,64 +1,28 @@
-﻿// 打家劫舍.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
-
-class Solution {
-public:
-    int lengthOfLIS(vector<int>& nums) {
-        int nAns[2506];
-        for (int j = 0; j < nums.size(); ++j) {
-            nAns[j] = 1;
-        }
-        for (int i = 1; i < nums.size(); ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[j] < nums[i])
-                    nAns[i] = max(nAns[i], nAns[j] + 1);
-            }
-        }
-        int nMax = 0;
-        for (const auto& data : nAns)
-            if (data > nMax)
-                nMax = data;
-        return nMax;
-    }
-};
-
-class Solution {
-public:
-    int lengthOfLIS(vector<int>& nums) {
-        int nAns[2506];
-        for (int j = 0; j < nums.size(); ++j)
-        {
-            nAns[j] = 1;
-        }
-        for (int i = 1; i < nums.size(); ++i)
-        {
-            for (int j = 1; j < i; ++j)
-            {
-                if (nums[j] > nums[i])
-                    nAns[i] = max(nAns[i], nAns[j] + 1);
-            }
-        }
-    }
-};
-
-int main()
+class Solution 
 {
-    std::cout << "Hello World!\n";
-}
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+public:
+    Solution()
+    {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+    }
+    int rob(vector<int>& nums) 
+    {
+        const auto &nSize = nums.size();
+        if(nSize==1)
+            return nums[0];
+        if(nSize==2)
+            return std::max(nums[0], nums[1]);
+        nums[2] += nums[0];
+        for (int i = 3; i < nSize;++i)
+        {
+            nums[i] += std::max(nums[i - 2], nums[i - 3]);
+        }
+        return std::max(nums[nSize - 1], nums[nSize - 2]);
+    }
+};
